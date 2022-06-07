17023b17a74ca7ce172f3a84a33f819fbda35f1b
Ankit Kumar <ankitkumar@Ankits-MacBook-Pro.local>
2022-06-07 19:35:33 +0530

    Added logic in service layer

--- src/main/java/com/Yebolo/controller/FetchNextNumberController.java
+++ src/main/java/com/Yebolo/controller/FetchNextNumberController.java
@@ -1,15 +1,21 @@
 package com.Yebolo.controller;
 
 import com.Yebolo.dto.Response;
+import com.Yebolo.service.FetchNextNumberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FetchNextNumberController {
 
-    @GetMapping("/fetchNextNumber")
-    public Response fetchNextNumber(){
+    @Autowired
+    private FetchNextNumberService fetchNextNumberService;
 
+    @GetMapping("/fetchNextNumber")
+    public Response fetchNextNumber(@RequestBody Integer number){
+        return fetchNextNumberService.fetchNextNumber(number);
     }
 
 }
