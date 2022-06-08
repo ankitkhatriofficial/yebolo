f6fb71635cbf15a32ddb186bc2225ca8d6ce0ad4
Ankit Kumar <ankitkumar@Ankits-MacBook-Pro.local>
2022-06-08 12:24:39 +0530

    changed get to post

--- src/main/java/com/Yebolo/controller/FetchNextNumberController.java
+++ src/main/java/com/Yebolo/controller/FetchNextNumberController.java
@@ -1,21 +1,22 @@
 package com.Yebolo.controller;
 
-import com.Yebolo.dto.Response;
-import com.Yebolo.service.FetchNextNumberService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.Yebolo.dto.Response;
+import com.Yebolo.service.FetchNextNumberService;
 
 @RestController
 public class FetchNextNumberController {
 
-    @Autowired
-    private FetchNextNumberService fetchNextNumberService;
+	@Autowired
+	private FetchNextNumberService fetchNextNumberService;
 
-    @GetMapping("/fetchNextNumber")
-    public Response fetchNextNumber(@RequestBody Integer number){
-        return fetchNextNumberService.fetchNextNumber(number);
-    }
+	@PostMapping("/fetchNextNumber")
+	public Response fetchNextNumber(@RequestBody Integer number) {
+		return fetchNextNumberService.fetchNextNumber(number);
+	}
 
 }
